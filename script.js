8bde6e039f42f800a828e20839ca76badcd12e8e
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2024-08-26 15:27:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const divEle = document.getElementById("browser-info");
+
+let version = navigator.appVersion;
+let name = navigator.appName
+divEle.innerText=`You are using ${name} version${version}`
